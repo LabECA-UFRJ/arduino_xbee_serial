5164067499822d3c4cff5ac35b747d8efe5f34d3
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-03-04 08:30:37 -0300

    Use xbee-arduino library instead of own solution

--- arduino_xbee_serial.ino
+++ arduino_xbee_serial.ino
@@ -3,12 +3,19 @@
 #include <OrangutanMotors.h>
 #include <OrangutanLCD.h>
 
+#include <Xbee.h>
+
 #include <avr/pgmspace.h>
+
+const int c_BaudRate = 57600;
 
 OrangutanLCD lcd;
 OrangutanMotors motors;
 OrangutanAnalog analog;
 OrangutanLEDs leds;
+
+XBee xbee = XBee();
+Rx16Response rx16 = Rx16Response();
 
 byte payload[3];
 int index;
@@ -32,14 +39,11 @@ void load_custom_characters()
 // 10 levels of bar graph characters
 const byte bar_graph_characters[10] = {' ',0,0,1,2,3,3,4,5,0xff};
 
-void setup() {
+void setup() 
+{
     load_custom_characters();
-    Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-    payload[0] = 0;
-    payload[1] = 0;
-    payload[2] = 0;
-    delay(1000);
-    index = 0;
+    Serial.begin(c_BaudRate);
+    xbee.setSerial(Serial);
 }
 
 void set_motor_speeds(int leftMotor, int rightMotor)
@@ -60,44 +64,47 @@ void show_speed_lcd(int leftSpeed, int rightSpeed)
     lcd.print("   ");
 }
 
-void loop() {
+void show_battery_lcd()
+{
     lcd.gotoXY(7, 0);
     char c = read_battery_millivolts_3pi() / 600;
     print_character(c);
-
-    while (Serial.available() != 0) {
-      char c = Serial.read();
-
-      if (c == 0x13) {
-        process_payload();
-      }
-
-      payload[0] = payload[1];
-      payload[1] = payload[2];
-      payload[2] = c;
-    }
 }
-// 7e 00 09 81 cc cc 30 00 00 00 13 a3
-/* Receive message
- *  Delimiter (0x7E)
- *  Length_16 (between length - checksum fields)
- *  Frame type (0x81)
- *  16bit source addr  
- *  RSSI_8 (signal strength)
- *  Options_8
- *  Rf data
- *  Checksum
- */
 
-void process_payload()
+void loop() 
 {
-  unsigned char command = payload[0] & 0x3F; // Command is 6 least significant bits.
-  unsigned char motor1IsReversed = (payload[0] & 0x80) >> 7; // Motor 1 dir is most significant bit.
-  unsigned char motor2IsReversed = (payload[0] & 0x40) >> 6; // Motor 2 dir is 2nd most significant bit.
+    show_battery_lcd();
 
-  int motor1Speed = payload[1] * (motor1IsReversed == 1 ? -1 : 1);
-  int motor2Speed = payload[2] * (motor2IsReversed == 1 ? -1 : 1);
+    bool dataAvailable = receive_xbee_data();
+    if (dataAvailable)
+        process_payload(rx16.getData(), rx16.getDataLength());
+}
 
-  show_speed_lcd(motor1Speed, motor2Speed);
-  set_motor_speeds(motor1Speed, motor2Speed);
+bool receive_xbee_data()
+{
+    xbee.readPacket();
+
+    if (xbee.getResponse().isAvailable() == false)
+        return false;
+
+    if (xbee.getResponse().getApiId() != RX_16_RESPONSE)
+        return false;
+
+    xbee.getResponse().getRx16Response(rx16);
+    return true;
+}
+
+// TODO: Check for other payload sizes.
+// e.g. command 0x01 will be a send me your battery level, so it only has 1 byte payload.
+void process_payload(uint8_t* data, uint8_t size)
+{
+    unsigned char command = data[0] & 0x3F;                    // Command is 6 least significant bits.
+    unsigned char motor1IsReversed = (data[0] & 0x80) >> 7;    // Motor 1 dir is most significant bit.
+    unsigned char motor2IsReversed = (data[0] & 0x40) >> 6;    // Motor 2 dir is 2nd most significant bit.
+
+    int motor1Speed = data[1] * (motor1IsReversed == 1 ? -1 : 1);
+    int motor2Speed = data[2] * (motor2IsReversed == 1 ? -1 : 1);
+
+    show_speed_lcd(motor1Speed, motor2Speed);
+    set_motor_speeds(motor1Speed, motor2Speed);
 }
